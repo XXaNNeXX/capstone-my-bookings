6bdd806be4f687ac69cfcba6d03dd7dfc93cd71d
neuefische <info@neuefische.de>
2023-10-20 12:33:52 +0200

    amend Booking number elements

--- backend/src/test/java/de/neuefische/backend/BookingServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/BookingServiceTest.java
@@ -9,17 +9,17 @@ class BookingServiceTest {
 
     BookingRepository bookingRepository = mock(BookingRepository.class);
     BookingService bookingService = new BookingService(bookingRepository);
-    Booking newBooking = new Booking(null,"Me", "01.01.2024", "05.01.2024", "2", "2");
+    Booking newBooking = new Booking(null,"Me", "01.01.2024", "05.01.2024", 2, 2);
 
     @Test
     void addBooking() {
         when(bookingRepository.save(newBooking))
-                .thenReturn(new Booking("1","Me", "01.01.2024", "05.01.2024", "2", "2"));
+                .thenReturn(new Booking("1","Me", "01.01.2024", "05.01.2024", 2, 2));
 
         Booking actual = bookingService.addBooking(newBooking);
 
         verify(bookingRepository).save(newBooking);
-        Booking expected = new Booking("1","Me", "01.01.2024", "05.01.2024", "2", "2");
+        Booking expected = new Booking("1","Me", "01.01.2024", "05.01.2024", 2, 2);
         assertEquals(expected, actual);
     }
 }
